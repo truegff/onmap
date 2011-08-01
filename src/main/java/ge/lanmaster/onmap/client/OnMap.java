06903ed1be2ff4dd1672de01a2b4d1ccde135cd1
Alexander Dolidze <alexander.dolidze@gmail.com>
2011-08-02 02:02:52 +0400

    release v1
    Signed-off-by: Alexander Dolidze <alexander.dolidze@gmail.com>

--- src/main/java/ge/lanmaster/onmap/client/OnMap.java
+++ src/main/java/ge/lanmaster/onmap/client/OnMap.java
@@ -1,14 +1,6 @@
 package ge.lanmaster.onmap.client;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.core.client.GWT;
-import com.google.gwt.user.client.Window;
-import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.google.gwt.user.client.rpc.ServiceDefTarget;
-import ge.lanmaster.gwt.ui.widgets.client.ABC;
-import ge.lanmaster.gwt.ui.widgets.client.MyExternalService;
-import ge.lanmaster.gwt.ui.widgets.client.MyExternalServiceAsync;
-
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -19,40 +11,7 @@ public class OnMap implements EntryPoint {
      * This is the entry point method.
      */
     public void onModuleLoad() {
-        ABC abc = new ABC();
-        Window.alert("I'm loaded " + ABC.className + " " + abc.one());
-
-        MyUserServiceAsync service = (MyUserServiceAsync) GWT.create(MyUserService.class);
-        ServiceDefTarget serviceDef = (ServiceDefTarget) service;
-        serviceDef.setServiceEntryPoint(GWT.getModuleBaseURL()+"userService");
-        service.sayHello(new AsyncCallback<String>() {
-
-            public void onFailure(Throwable caught) {
-                Window.alert("failure "+caught.getMessage());
-                //throw new UnsupportedOperationException("Not supported yet.");
-            }
-
-            public void onSuccess(String result) {
-                Window.alert(result);
-                //throw new UnsupportedOperationException("Not supported yet.");
-            }
-        });
-
-        MyExternalServiceAsync service2 = (MyExternalServiceAsync) GWT.create(MyExternalService.class);
-        ServiceDefTarget serviceDef2 = (ServiceDefTarget) service;
-        serviceDef2.setServiceEntryPoint(GWT.getHostPageBaseURL()+"externalService");
-        service2.sayHowdy(new AsyncCallback<String>() {
-
-            public void onFailure(Throwable caught) {
-                Window.alert("failure "+caught.getMessage());
-                //throw new UnsupportedOperationException("Not supported yet.");
-            }
-
-            public void onSuccess(String result) {
-                Window.alert(result);
-                //throw new UnsupportedOperationException("Not supported yet.");
-            }
-        });
+        //Do nothing
     }
 
 }
