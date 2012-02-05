1d4139c16bc74f14767e6a274447cf27749ca161
Alexander Dolidze <alexander.dolidze@gmail.com>
2012-02-05 18:29:18 +0400

    introducing new servlet, unfinished yet though

---
+++ src/main/java/ge/lanmaster/onmap/root/client/services/DataTransferService.java
@@ -0,0 +1,8 @@
+package ge.lanmaster.onmap.root.client.services;
+
+import com.google.gwt.user.client.rpc.RemoteService;
+
+//@RemoteSeviceRelativePath(value = "dts")
+public interface DataTransferService extends RemoteService {
+    //public MapConfig getMapConfig(String );
+}
---
+++ src/main/java/ge/lanmaster/onmap/root/server/service/DataTransferServiceImpl.java
@@ -0,0 +1,17 @@
+package ge.lanmaster.onmap.root.server.service;
+
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+import ge.lanmaster.onmap.root.client.services.DataTransferService;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: alexander
+ * Date: 2/3/12
+ * Time: 5:30 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class DataTransferServiceImpl extends RemoteServiceServlet implements DataTransferService {
+//    public MapConfig getMapConfig() {
+//        return null;  //To change body of implemented methods use File | Settings | File Templates.
+//    }
+}
