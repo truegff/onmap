3b5d779c3d1b935f5de95a73c0f0cc8eeadec7cb
Alexander Dolidze <alexander.dolidze@gmail.com>
2012-02-11 21:14:49 +0400

    edited MapConfigManager

--- src/main/java/ge/lanmaster/onmap/root/client/manager/MapConfigManager.java
+++ src/main/java/ge/lanmaster/onmap/root/client/manager/MapConfigManager.java
@@ -1,28 +1,32 @@
 package ge.lanmaster.onmap.root.client.manager;
 
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.Command;
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import ge.lanmaster.onmap.root.client.entity.MapConfig;
+import ge.lanmaster.onmap.root.client.services.DataTransferService;
+import ge.lanmaster.onmap.root.client.services.DataTransferServiceAsync;
 
 public class MapConfigManager {
     private MapConfig mapConfig;
 
-    public void retrieveMapConfig(Command successCase, Command failureCase) {
-//        LoginServiceAsync loginService = GWT.create(LoginService.class);
-//        GWT.log("GWT.getHostPageBaseURL() = " + GWT.getHostPageBaseURL());
-//        try {
-//            loginService.login(GWT.getHostPageBaseURL(), new AsyncCallback<UserState>()  {
-//                public void onFailure(Throwable caught) {
-//                    failureCase.execute();
-//                }
-//                public void onSuccess(UserState result) {
-//                    userState = result;
-//                    successCase.execute();
-//                }
-//            });
-//        } catch (Exception e) {
-//            failureCase.execute();
-//            GWT.log(e.getMessage());
-//        }
+    public void retrieveMapConfig(final Command successCase, final Command failureCase) {
+        DataTransferServiceAsync service = GWT.create(DataTransferService.class);
+        try {
+            service.getMapConfig(new AsyncCallback<MapConfig>() {
+                public void onFailure(Throwable caught) {
+                    failureCase.execute();
+                }
+
+                public void onSuccess(MapConfig result) {
+                    mapConfig = result;
+                    successCase.execute();
+                }
+            });
+        } catch (Exception e) {
+            failureCase.execute();
+            GWT.log(e.getMessage());
+        }
     }
 
     public MapConfig getMapConfig() {
