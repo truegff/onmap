db97d62e3ed6c0690c47b41fa4557d12f244eb28
Alexander Dolidze <alexander.dolidze@gmail.com>
2012-02-23 22:05:03 +0400

    getting UserStateManager using gin, step1

--- src/main/java/ge/lanmaster/onmap/root/client/gin/GinClassBindingModule.java
+++ src/main/java/ge/lanmaster/onmap/root/client/gin/GinClassBindingModule.java
@@ -4,11 +4,13 @@ import com.google.gwt.inject.client.AbstractGinModule;
 import com.google.inject.Singleton;
 import ge.lanmaster.onmap.root.client.ClientFactory;
 import ge.lanmaster.onmap.root.client.ClientFactoryImpl;
+import ge.lanmaster.onmap.root.client.manager.UserStateManager;
 
 public class GinClassBindingModule extends AbstractGinModule {
 
     @Override
     protected void configure() {
         bind(ClientFactory.class).to(ClientFactoryImpl.class).in(Singleton.class);
+        bind(UserStateManager.class).in(Singleton.class);
     }
 }
--- src/main/java/ge/lanmaster/onmap/root/client/gin/GinFactory.java
+++ src/main/java/ge/lanmaster/onmap/root/client/gin/GinFactory.java
@@ -2,10 +2,12 @@ package ge.lanmaster.onmap.root.client.gin;
 
 import com.google.gwt.inject.client.GinModules;
 import com.google.gwt.inject.client.Ginjector;
-import com.google.web.bindery.event.shared.EventBus;
 import ge.lanmaster.onmap.root.client.ClientFactory;
+import ge.lanmaster.onmap.root.client.manager.UserStateManager;
 
 @GinModules({GinClassBindingModule.class})
 public interface GinFactory extends Ginjector {
     ClientFactory getClientFactory();
+
+    UserStateManager getUserStateManager();
 }
