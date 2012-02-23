7e319b7e74a5e881145b06622d49323ee19d82dc
Alexander Dolidze <alexander.dolidze@gmail.com>
2012-02-23 14:57:00 +0400

    tried to implement GIN instead of clientFactory, attempt failed. will start over in new branch step by step.

---
+++ src/main/java/ge/lanmaster/onmap/root/client/gin/GinClassBindingModule.java
@@ -0,0 +1,11 @@
+package ge.lanmaster.onmap.root.client.gin;
+
+import com.google.gwt.inject.client.AbstractGinModule;
+
+public class GinClassBindingModule extends AbstractGinModule {
+
+    @Override
+    protected void configure() {
+
+    }
+}
---
+++ src/main/java/ge/lanmaster/onmap/root/client/gin/GinFactory.java
@@ -0,0 +1,10 @@
+package ge.lanmaster.onmap.root.client.gin;
+
+import com.google.gwt.inject.client.GinModules;
+import com.google.gwt.inject.client.Ginjector;
+
+@GinModules({GinClassBindingModule.class})
+public interface GinFactory extends Ginjector {
+
+
+}
