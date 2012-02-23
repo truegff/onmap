684556a678f4d2ea2a3e056150c5cde80d93c853
Alexander Dolidze <alexander.dolidze@gmail.com>
2012-02-23 18:57:53 +0400

    added singleton EventBus fetching instruction to gin.

--- src/main/java/ge/lanmaster/onmap/root/client/gin/GinClassBindingModule.java
+++ src/main/java/ge/lanmaster/onmap/root/client/gin/GinClassBindingModule.java
@@ -1,11 +1,14 @@
 package ge.lanmaster.onmap.root.client.gin;
 
 import com.google.gwt.inject.client.AbstractGinModule;
+import com.google.inject.Singleton;
+import com.google.web.bindery.event.shared.EventBus;
+import com.google.web.bindery.event.shared.SimpleEventBus;
 
 public class GinClassBindingModule extends AbstractGinModule {
 
     @Override
     protected void configure() {
-
+        bind(EventBus.class).to(SimpleEventBus.class).in(Singleton.class);
     }
 }
--- src/main/java/ge/lanmaster/onmap/root/client/gin/GinFactory.java
+++ src/main/java/ge/lanmaster/onmap/root/client/gin/GinFactory.java
@@ -2,9 +2,9 @@ package ge.lanmaster.onmap.root.client.gin;
 
 import com.google.gwt.inject.client.GinModules;
 import com.google.gwt.inject.client.Ginjector;
+import com.google.web.bindery.event.shared.EventBus;
 
 @GinModules({GinClassBindingModule.class})
 public interface GinFactory extends Ginjector {
-
-
+    EventBus getEventBus();
 }
